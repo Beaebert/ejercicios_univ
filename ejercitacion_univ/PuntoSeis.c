f125eae697977b6332e6cf1546142a524a72242a
Beatriz Ebert <ebert.beatriz.be@gmail.com>
2025-03-17 16:13:20 -0300

    resuelto hasta el punto 6

--- ejercitacion_univ/PuntoSeis.c
+++ ejercitacion_univ/PuntoSeis.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define DIA
 
 // Punto 6 de la tarea
 int main(void) {
     int Lado1, Lado2 = 0;
 
-    printf("Ingrese el primer lado del triangulo rectangulo\n");
+    printf("Ingrese el lado mas largo del rectangulo\n");
     scanf("%i", &Lado1);
-    printf("Ingrese el segundo lado del triangulo rectangulo\n");
+    printf("Ingrese el lado mas corto del rectangulo\n");
     scanf("%i", &Lado2);
 
-    int Hipotenusa = Lado1^2 + Lado2^2;
-    int Perimetro = Lado1 + Lado2 + Hipotenusa;
-    int Superficie = (Lado1 * Lado2) /2;
-    printf("El perimetro del triangulo es %i \n", Hipotenusa);
-    printf("El perimetro del triangulo es %i \n", Perimetro);
-    printf("El perimetro del triangulo es %i \n", Superficie);
+    float DiagonalP = sqrt(Lado1^2 + Lado2^2);
+    int Perimetro = Lado1 * 2 + Lado2 * 2;
+    int Superficie = Lado1 * Lado2;
+    printf("El Diagonal del rectangulo es %i \n", DiagonalP);
+    printf("El Perimetro del rectangulo es %i \n", Perimetro);
+    printf("El Area del rectangulo es %i \n", Superficie);
     return 0;
 }
